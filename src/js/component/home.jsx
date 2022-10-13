f2edb1ad6ddc83537ca5ae3bd3e3dfbd022e2125
MireyaCR <mireya.mcr@gmail.com>
2022-10-13 12:56:46 +0000

    ReactLandingPage

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,22 +3,50 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+const Navbar = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+			<a className="navbar-brand" href="#">Start Bootstap</a>
+			<div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo01">
+				<a className="navbar-brand"></a>
+					<ul className="navbar-nav mr-auto mt-2 mt-lg-0" href="#">
+					<li className="nav-item active">
+						<a className="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+					</li>
+					<li className="nav-item ">
+						<a className="nav-link" href="#">About</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Services</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">Contact</a>
+					</li>	  			
+				</ul>
+    		</div>
+		</nav>		
+	);
+};
+
+const JumBotron = () => {
+	return (
+        <div className="jumbotron m-5">
+			<h1 className="display-4"></h1>
+			<p className="lead"></p>
+			<a className="btn btn-primary btn-lg" href="" role="button"></a>
+		</div>
+    )
+ };
+
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<Navbar/>
+			<JumBotron/>
 		</div>
 	);
 };
