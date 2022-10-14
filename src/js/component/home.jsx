ba903dfe357cceaaa8d3271e8b05225fd1261818
MireyaCR <mireya.mcr@gmail.com>
2022-10-14 16:20:56 +0000

    ReactLandingPage

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -40,6 +40,24 @@ const JumBotron = () => {
 		</div>
     )
  };
+ const Cartas=()=>{
+	return(
+		<div class="row row-cols-1 row-cols-md-3 g-4">
+			<div class="col">
+				<div class="card h-100">
+					<img src="..." class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					</div>
+					<div class="card-footer">
+						<small class="text-muted">Last updated 3 mins ago</small>
+					</div>
+				</div>
+  			</div>
+		</div>
+	)
+ };
 
 //create your first component
 const Home = () => {
@@ -47,6 +65,7 @@ const Home = () => {
 		<div>
 			<Navbar/>
 			<JumBotron/>
+			<Cartas/>
 		</div>
 	);
 };
