d34a151b1a48e9456476b75d35b3c3b1ace25a19
MireyaCR <mireya.mcr@gmail.com>
2023-01-10 09:26:50 +0000

    barra fija

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Navbar = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
 			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
